bc621776c506db87e90d59c83ceb83a0ad4097f5
Dylan Kok <me@dylankjy.com>
2023-01-02 20:19:16 +0800

    Set body limit to 5mb in api

--- foundit-rest-api/app.js
+++ foundit-rest-api/app.js
@@ -4,7 +4,7 @@ const env = require('./app/config')
 // Express
 const express = require('express')
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '5mb' }))
 
 // Rate limiting
 const rateLimit = require('express-rate-limit')
