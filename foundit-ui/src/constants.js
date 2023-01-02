7eb1d2436afe7dbb8934c6cdb299cd52dd774fbc
Dylan Kok <me@dylankjy.com>
2023-01-02 17:58:32 +0800

    Add itemCategories into constants

--- foundit-ui/src/constants.js
+++ foundit-ui/src/constants.js
@@ -1,6 +1,15 @@
 const constants = {
     NAME: 'Foundit',
     REST_API_ENDPOINT: 'http://localhost:5000/api',
+
+    itemCategories: [
+        { id: 1, name: 'Personal Items' },
+        { id: 2, name: 'Books' },
+        { id: 3, name: 'Stationary' },
+        { id: 4, name: 'Wallets' },
+        { id: 5, name: 'Smart devices' },
+        { id: 6, name: 'Bags' }
+    ]
 }
 
 export default constants
