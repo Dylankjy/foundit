95856768cb87dc64701da75bdec95ef7a5f561a5
Dylan Kok <me@dylankjy.com>
2023-01-19 22:06:20 +0800

    Modify dockerfile and build script

--- foundit-rest-api/publish_docker.cmd
+++ foundit-rest-api/publish_docker.cmd
@@ -1,5 +1,4 @@
 @echo off
-LABEL org.opencontainers.image.source https://github.com/dylankjy/foundit
 
 docker build . --tag ghcr.io/dylankjy/foundit-rest-api:latest
 docker push ghcr.io/dylankjy/foundit-rest-api:latest
